765bb9e45448da6b33a24f93c898e4f555d856dc
Greg Naughton <greg@naughton.org>
2013-12-17 21:47:21 -0700

    updated unit tests to reflect _id placement at beginning of rows

--- tests/unit_tests.js
+++ tests/unit_tests.js
@@ -47,7 +47,6 @@ UnitTest.prototype = {
 
 var db_name = "MONGO-UNITTEST.db";
 var U = new UnitTest();
-//process.on('exit',U.report);
 process.on('exit',function() { U.report(); });
 
 if ( U.fs.existsSync( db_name ) ) {
@@ -89,42 +88,42 @@ U.test( "db.insert({c:7})", 7 );
 
 U.p( "\nFIND TESTS:" );
 U.p( "simple find:" );
-U.test( "db.find({a:1})", '{"length":1,"_data":[{"a":1,"_id":1}]}' );
+U.test( "db.find({a:1})", '{"length":1,"_data":[{"_id":1,"a":1}]}' );
 U.p( "simple $gt:" );
-U.test( "db.find({b:{'$gt':4}})", '{"length":1,"_data":[{"b":5,"c":5,"d":5,"_id":5}]}' );
+U.test( "db.find({b:{'$gt':4}})", '{"length":1,"_data":[{"_id":5,"b":5,"c":5,"d":5}]}' );
 U.p( "simple $lt:" );
-U.test( "db.find({b:{'$lt':4}})", '{"length":2,"_data":[{"a":2,"b":2,"_id":2},{"a":3,"b":3,"c":3,"_id":3}]}' );
+U.test( "db.find({b:{'$lt':4}})", '{"length":2,"_data":[{"_id":2,"a":2,"b":2},{"_id":3,"a":3,"b":3,"c":3}]}' );
 U.p( "simple $gte:" );
-U.test( "db.find({b:{'$gte':4}})", '{"length":2,"_data":[{"a":4,"b":4,"c":4,"d":4,"_id":4},{"b":5,"c":5,"d":5,"_id":5}]}' );
+U.test( "db.find({b:{'$gte':4}})", '{"length":2,"_data":[{"_id":4,"a":4,"b":4,"c":4,"d":4},{"_id":5,"b":5,"c":5,"d":5}]}' );
 U.p( "simple $lte:" );
-U.test( "db.find({b:{'$lte':4}})", '{"length":3,"_data":[{"a":2,"b":2,"_id":2},{"a":3,"b":3,"c":3,"_id":3},{"a":4,"b":4,"c":4,"d":4,"_id":4}]}' );
+U.test( "db.find({b:{'$lte':4}})", '{"length":3,"_data":[{"_id":2,"a":2,"b":2},{"_id":3,"a":3,"b":3,"c":3},{"_id":4,"a":4,"b":4,"c":4,"d":4}]}' );
 U.p( "reverse sort, numeric:" );
-U.test( "db.find({b:{'$gte':4}}).sort({'_id':-1})", '{"length":2,"_data":[{"b":5,"c":5,"d":5,"_id":5},{"a":4,"b":4,"c":4,"d":4,"_id":4}]}' );
+U.test( "db.find({b:{'$gte':4}}).sort({'_id':-1})", '{"length":2,"_data":[{"_id":5,"b":5,"c":5,"d":5},{"_id":4,"a":4,"b":4,"c":4,"d":4}]}' );
 U.test( "db.insert({name:'Paul'})", 8 );
 U.test( "db.insert({name:'Carol'})", 9 );
 U.test( "db.insert({name:'Zach'})", 10 );
 U.p( "\nALPHABETICAL SORT:" );
-U.test( "db.find({name:/(.*)/}).sort({name:1})", '{"length":3,"_data":[{"name":"Carol","_id":9},{"name":"Paul","_id":8},{"name":"Zach","_id":10}]}' );
+U.test( "db.find({name:/(.*)/}).sort({name:1})", '{"length":3,"_data":[{"_id":9,"name":"Carol"},{"_id":8,"name":"Paul"},{"_id":10,"name":"Zach"}]}' );
 U.p( "$exists:true:" );
-U.test( "db.find({name:{'$exists':true}}).sort({name:-1})", '{"length":3,"_data":[{"name":"Zach","_id":10},{"name":"Paul","_id":8},{"name":"Carol","_id":9}]}' );
+U.test( "db.find({name:{'$exists':true}}).sort({name:-1})", '{"length":3,"_data":[{"_id":10,"name":"Zach"},{"_id":8,"name":"Paul"},{"_id":9,"name":"Carol"}]}' );
 U.p( "\n$exists:false && $exists:false:" );
-U.test( "db.find({name:{'$exists':false},'a':{'$exists':false},'b':{'$exists':false}}).sort({_id:-1})", '{"length":2,"_data":[{"c":7,"_id":7},{"c":6,"d":6,"_id":6}]}' );
+U.test( "db.find({name:{'$exists':false},'a':{'$exists':false},'b':{'$exists':false}}).sort({_id:-1})", '{"length":2,"_data":[{"_id":7,"c":7},{"_id":6,"c":6,"d":6}]}' );
 
 U.p( "\nAND $exists:true:" );
-U.test( "db.find({a:{$exists:true},b:{$exists:true},c:{$exists:true},d:{$exists:true}})", '{"length":1,"_data":[{"a":4,"b":4,"c":4,"d":4,"_id":4}]}' );
+U.test( "db.find({a:{$exists:true},b:{$exists:true},c:{$exists:true},d:{$exists:true}})", '{"length":1,"_data":[{"_id":4,"a":4,"b":4,"c":4,"d":4}]}' );
 U.p( "\nOR:" );
-U.test( "db.find({$or:[{d:{$exists:true}},{c:{$exists:true}}]}).sort({_id:-1})", '{"length":5,"_data":[{"c":7,"_id":7},{"c":6,"d":6,"_id":6},{"b":5,"c":5,"d":5,"_id":5},{"a":4,"b":4,"c":4,"d":4,"_id":4},{"a":3,"b":3,"c":3,"_id":3}]}' );
+U.test( "db.find({$or:[{d:{$exists:true}},{c:{$exists:true}}]}).sort({_id:-1})", '{"length":5,"_data":[{"_id":7,"c":7},{"_id":6,"c":6,"d":6},{"_id":5,"b":5,"c":5,"d":5},{"_id":4,"a":4,"b":4,"c":4,"d":4},{"_id":3,"a":3,"b":3,"c":3}]}' );
 
 U.p( "\nAND CONDITIONAL:" );
-U.test( "db.find({b:{$gt:3},c:{$lte:5},d:{$lt:6},b:{$gte:5}})", '{"length":1,"_data":[{"b":5,"c":5,"d":5,"_id":5}]}' );
+U.test( "db.find({b:{$gt:3},c:{$lte:5},d:{$lt:6},b:{$gte:5}})", '{"length":1,"_data":[{"_id":5,"b":5,"c":5,"d":5}]}' );
 
 U.p( "\nOR CONDITIONAL:" );
-U.test( "db.find( {a:{$exists:true},b:{$lt:3},a:{$gte:1} } )", '{"length":1,"_data":[{"a":2,"b":2,"_id":2}]}' );
-U.test( "db.find( {b:{$gt:4},c:{$gte:5},d:{$lte:6}})", '{"length":1,"_data":[{"b":5,"c":5,"d":5,"_id":5}]}' );
-U.test( "db.find( {a:3,b:{$gt:2},c:{$lt:7}} )", '{"length":1,"_data":[{"a":3,"b":3,"c":3,"_id":3}]}' );
+U.test( "db.find( {a:{$exists:true},b:{$lt:3},a:{$gte:1} } )", '{"length":1,"_data":[{"_id":2,"a":2,"b":2}]}' );
+U.test( "db.find( {b:{$gt:4},c:{$gte:5},d:{$lte:6}})", '{"length":1,"_data":[{"_id":5,"b":5,"c":5,"d":5}]}' );
+U.test( "db.find( {a:3,b:{$gt:2},c:{$lt:7}} )", '{"length":1,"_data":[{"_id":3,"a":3,"b":3,"c":3}]}' );
 
 U.p( "\nAND/OR CONDITIONAL COMBINED:" );
-U.test( "db.find( { c:{$lt:6}, $or: [{a:{$gt:5}},{b:{$lte:4}}] } )", '{"length":2,"_data":[{"a":3,"b":3,"c":3,"_id":3},{"a":4,"b":4,"c":4,"d":4,"_id":4}]}' );
+U.test( "db.find( { c:{$lt:6}, $or: [{a:{$gt:5}},{b:{$lte:4}}] } )", '{"length":2,"_data":[{"_id":3,"a":3,"b":3,"c":3},{"_id":4,"a":4,"b":4,"c":4,"d":4}]}' );
 
 U.p( "\nREMOVE: ");
 U.test( "db.remove( {name:/[A-Z](.*)/} )", 3 );
@@ -178,4 +177,3 @@ U.test("A=[];db.find(/.*/).skip(40).limit(20)._data.forEach(function(o){A.push(o
 */
 U.p( "\nDISTINCT: " );
 
-//U.report();
